bb1d42d7458268df2ab97433b76538ff26e277fa
Jiseong Kim <kim.intelligence@gmail.com>
2020-06-11 03:10:34 +0900

    preprocessing -> HRV, LF, HF, LH/HF 추가 -> 데이터 타입 변화로 인해 제피어 데이터 전부 무시 -> 제피어 관련 코드 전부 주석처리

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -18,10 +18,12 @@ import math
 import statistics
 import numpy
 from naming import Change_All_Datafiles_Name as Change
+import matplotlib.pyplot as plt
 
 ADDED_TIME = 0 # added to label.end_time
 count = {}
 count_total = 0
+end_idx = -1
 
 # cur_time ex) 0728121132 (no year) 한국시간 기준으로 넣으면 됨
 def get_UTC_time(cur_time):
@@ -39,14 +41,15 @@ def get_UTC_time_from_zephyr(cur_time):
 def get_start_idx(UTC_time, latest_time, HZ):
 	return int((UTC_time - latest_time) * HZ)
 
+'''
 def get_start_RR_idx(start_time, zephyr_time, cnt):
 	start_idx = -1
-	sum_column = zephyr_time
+	sum_column = 0
 	while True:
 		start_idx = start_idx + 1
 		if start_idx>=cnt:
 			break
-		sum_column = sum_column + int(rr_df[column_name['RtoR']][start_idx])/1000
+		sum_column = sum_column + rr_df[column_name['RtoR']][start_idx]/1000
 		if start_time < sum_column:
 			break
 	return start_idx
@@ -69,7 +72,7 @@ def get_HRV(start_idx, end_idx):
 	for idx in range(start_idx, end_idx + 1):
 		sum_diff_square = sum_diff_square + (int(rr_df[column_name['RtoR']][idx+1])-int(rr_df[column_name['RtoR']][idx]))*(int(rr_df[column_name['RtoR']][idx+1])-int(rr_df[column_name['RtoR']][idx]))/1000000
 	return math.sqrt(sum_diff_square/cnt)
-
+'''
 
 def check_noise(STFW, latest_time):
     #TODO:
@@ -104,7 +107,7 @@ def check_noise(STFW, latest_time):
                 if hr_df[column_name['eda']][i] == 0:
                     hr_df[column_name['eda']][i] = mean
 
-    idx = get_start_idx(STFW, latest_time, hz['breathing'])
+    '''idx = get_start_idx(STFW, latest_time, hz['breathing'])
     if breathing_df[column_name['breathing']][idx:idx + hz['breathing'] * time_window].min() == 0:
         if noise_mode == 1 and 'breathing' in count:
             count['breathing'] += 1
@@ -132,7 +135,7 @@ def check_noise(STFW, latest_time):
             for i in range(idx, idx + hz['ecg'] * time_window):
                 if hr_df[column_name['ecg']][i] == 0:
                     hr_df[column_name['ecg']][i] = mean
-
+'''
     ################################## address noise #####################################
     # 1. mediate the noise data
     # 2. just delete
@@ -143,16 +146,16 @@ def check_noise(STFW, latest_time):
 # set data from each file from start_time to end_time
 # if (start_time - end_time) % time_window != 0: just ignore the tail
 def set_data(start_time, end_time, zephyr_time, time_window, emotion):
-	global count_total
+	global count_total, end_idx
 	for STFW in range(start_time, end_time, time_window): # STFW: start time for window
 		if STFW + time_window > end_time: # time-window size is not enough
 			break
 
 		count_total += 1
-
+		'''
 		if check_noise(STFW, latest_time): # TODO
 			continue
-
+		'''
 		labeling.append(emotion)
 
 		# hr
@@ -170,7 +173,11 @@ def set_data(start_time, end_time, zephyr_time, time_window, emotion):
 		eda_avg.append(eda_df[column_name['eda']][idx:idx + hz['eda']*time_window].sum() / (time_window * hz['eda']))
 		eda_med.append(statistics.median(eda_df[column_name['eda']][idx:idx + hz['eda']*time_window]))
 		eda_std.append(numpy.std(eda_df[column_name['eda']][idx:idx + hz['eda']*time_window]))
-		
+
+		# sbp
+		idx = get_start_idx(STFW, latest_time, hz['bvp'])
+		sbp.append(bvp_df[column_name['bvp']][idx:idx + hz['bvp']*time_window].max())
+		'''		
 		# breathing
 		idx = get_start_idx(STFW, latest_time, hz['breathing'])
 		breathing_max.append(breathing_df[column_name['breathing']][idx:idx + hz['breathing']*time_window].max())
@@ -186,19 +193,36 @@ def set_data(start_time, end_time, zephyr_time, time_window, emotion):
 		ecg_avg.append(ecg_df[column_name['ecg']][idx:idx + hz['ecg']*time_window].sum() / (time_window * hz['ecg']))
 		ecg_med.append(statistics.median(ecg_df[column_name['ecg']][idx:idx + hz['ecg']*time_window]))
 		ecg_std.append(numpy.std(ecg_df[column_name['ecg']][idx:idx + hz['ecg']*time_window]))
-
+		'''
+		'''
 		# hrv
-		rr_cnt = int(rr_df['RtoR'].count())
-		start_idx = get_start_RR_idx(STFW, zephyr_time, rr_cnt)
-		end_idx = get_end_RR_idx(STFW, time_window, start_idx, zephyr_time, rr_cnt)
-		hrv.append(get_HRV(start_idx, end_idx))
 
-		# sbp
-		idx = get_start_idx(STFW, latest_time, hz['bvp'])
-		sbp.append(bvp_df[column_name['bvp']][idx:idx + hz['bvp']*time_window].max())
+		hrv_interval = 1
+		hrv_window = 120
+		hrv = []
+
+		for STFW__ in range(STFW - int(zephyr_time), STFW + time_window - int(zephyr_time), hrv_interval):
+			start_idx = get_start_RR_idx(STFW__ - hrv_window, zephyr_time, len(rr_df) - 1)
+			end_idx = get_end_RR_idx(STFW__ - hrv_window, hrv_window, start_idx, 0, len(rr_df) - 1)
+			# 실제 rr_diff_square는 한칸씩 밀려있는 거 고려 해줘야 함
+			hrv.append(math.sqrt(sum(rr_diff_square[start_idx:end_idx]) / (end_idx - start_idx + 1)))
+
+
+		n = len(hrv)  # Length of signal
+		NFFT = n  # ?? NFFT=2^nextpow2(length(y))  ??
+
+		Y = numpy.fft.fft(hrv) / NFFT  # fft computing and normaliation
+		Y = Y[range(math.trunc(NFFT / 2))]  # single sied frequency range
+		amplitude_Hz = 4 * abs(Y) ** 2
+
+		LF.append(sum(amplitude_Hz[int(0.04 * 240 + 0.5):int(0.15 * 240 + 0.5)]))
+		HF.append(sum(amplitude_Hz[int(0.14 * 240 + 0.5):int(0.4 * 240 + 0.5)]))
+		#LF_HF_ratio.append(sum(amplitude_Hz[int(0.04 * 240 + 0.5):int(0.15 * 240 + 0.5)])/float(sum(amplitude_Hz[int(0.14 * 240 + 0.5):int(0.4 * 240 + 0.5)])))
+		'''
+
 
 Change()
-		
+
 ##################################### get inputs #####################################
 # folder name
 dir_name = input("Input your folder name: ")
@@ -231,9 +255,9 @@ label = pandas.read_csv(os.path.join(this_dir, dir_name, 'LABEL.csv'))
 hr = pandas.read_csv(os.path.join(this_dir, dir_name, 'HR.csv'))
 eda = pandas.read_csv(os.path.join(this_dir, dir_name, 'EDA.csv'))
 ibi = pandas.read_csv(os.path.join(this_dir, dir_name, 'IBI.csv'))
-breathing = pandas.read_csv(os.path.join(this_dir, dir_name, 'BREATHING.csv'))
-ecg = pandas.read_csv(os.path.join(this_dir, dir_name, 'ECG.csv'))
-rr = pandas.read_csv(os.path.join(this_dir, dir_name,'RR.csv'))	# zephyr랑 empatica data_directory sync 문제
+#breathing = pandas.read_csv(os.path.join(this_dir, dir_name, 'BREATHING.csv'))
+#ecg = pandas.read_csv(os.path.join(this_dir, dir_name, 'ECG.csv'))
+# rr = pandas.read_csv(os.path.join(this_dir, dir_name,'RR.csv'))	# zephyr랑 empatica data_directory sync 문제
 bvp = pandas.read_csv(os.path.join(this_dir, dir_name, 'BVP.csv'))
 
 all_dataFrame = []
@@ -258,8 +282,12 @@ ecg_min = []
 ecg_avg = []
 ecg_med = []
 ecg_std = []
-hrv = []
+#hrv = []
+#LF = []
+#HF = []
+#LF_HF_ratio = []
 sbp = []
+#rr_diff_square = []
 
 # read data as dataFrame
 #hr_df = pandas.DataFrame(hr)
@@ -268,9 +296,9 @@ sbp = []
 hr_df = hr
 eda_df = eda
 ibi_df = ibi
-breathing_df = breathing
-ecg_df = ecg
-rr_df = rr
+#breathing_df = breathing
+#ecg_df = ecg
+#rr_df = rr
 bvp_df = bvp
 
 # get the column name
@@ -278,11 +306,19 @@ column_name = {}
 column_name['hr'] = list(hr_df)[0]
 column_name['eda'] = list(eda_df)[0]
 column_name['ibi'] = list(ibi_df)[0]
-column_name['breathing'] = list(breathing_df)[1]
-column_name['ecg'] = list(ecg_df)[1]
-column_name['RtoR'] = list(rr_df)[1]
-column_name['BVP'] = list(bvp_df)[1]
+#column_name['breathing'] = list(breathing_df)[1]
+#column_name['ecg'] = list(ecg_df)[1]
+#column_name['RtoR'] = list(rr_df)[1]
+column_name['bvp'] = list(bvp_df)[0]
 
+'''
+for i in range(1, len(rr_df), 1):
+    if rr_df[column_name['RtoR']][i] > 1400 or rr_df[column_name['RtoR']][i] < 450:
+        rr_df[column_name['RtoR']][i] = (rr_df[column_name['RtoR']][i - 1] + rr_df[column_name['RtoR']][i + 1]) / 2
+
+for i in range(len(rr_df) - 1):
+    rr_diff_square.append((rr_df[column_name['RtoR']][i + 1] - rr_df[column_name['RtoR']][i]) ** 2)
+'''
 ########################### Adjusting the START & END time ###########################
 
 # get the start time of all the data as Empatica form(UNIX UTC)
@@ -290,8 +326,8 @@ start_time = {}
 start_time['hr'] = float(list(hr_df)[0])
 start_time['eda'] = float(list(eda_df)[0])
 start_time['ibi'] = float(list(ibi_df)[0])
-start_time['breathing'] = float(get_UTC_time_from_zephyr(breathing_df['Time'][0]))
-start_time['ecg'] = float(get_UTC_time_from_zephyr(ecg_df['Time'][0]))
+#start_time['breathing'] = float(get_UTC_time_from_zephyr(breathing_df['Time'][0]))
+#start_time['ecg'] = float(get_UTC_time_from_zephyr(ecg_df['Time'][0]))
 start_time['bvp'] = float(list(bvp_df)[0])
 
 # get HZ
@@ -304,8 +340,8 @@ hz['ecg'] = 250
 hz['bvp'] = 64
 
 # get the latest start time in sec
-latest_time = max(start_time['hr'], start_time['eda'], start_time['ibi'], start_time['breathing'], start_time['ecg'], start_time['bvp'])
-zephyr_time = max(start_time['breathing'], start_time['ecg'])
+latest_time = max(start_time['hr'], start_time['eda'], start_time['ibi'], start_time['bvp'])
+#zephyr_time = max(start_time['breathing'], start_time['ecg'])
 
 ######################## drop the data before latest time ############################
 
@@ -314,8 +350,8 @@ time_diff = {}
 time_diff['hr'] = latest_time - start_time['hr']
 time_diff['eda'] = latest_time - start_time['eda']
 time_diff['ibi'] = latest_time - start_time['ibi']
-time_diff['breathing'] = latest_time - start_time['breathing']
-time_diff['ecg'] = latest_time - start_time['ecg']
+#time_diff['breathing'] = latest_time - start_time['breathing']
+#time_diff['ecg'] = latest_time - start_time['ecg']
 time_diff['bvp'] = latest_time - start_time['bvp']
 
 # get the number of rows that should be deleted
@@ -323,20 +359,20 @@ del_num = {}
 del_num['hr'] = time_diff['hr'] * hz['hr']
 del_num['eda'] = time_diff['eda'] * hz['eda']
 del_num['ibi'] = time_diff['ibi'] * hz['ibi']
-del_num['breathing'] = time_diff['breathing'] * hz['breathing']
-del_num['ecg'] = time_diff['ecg'] * hz['ecg']
+#del_num['breathing'] = time_diff['breathing'] * hz['breathing']
+#del_num['ecg'] = time_diff['ecg'] * hz['ecg']
 del_num['bvp'] = time_diff['bvp'] * hz['bvp']
 
 # delete rows as much as the difference (also row for Hz)
 hr_df.drop([i for i in range(int(del_num['hr']) + 1)], inplace=True)
 eda_df.drop([i for i in range(int(del_num['eda']) + 1)], inplace=True)
 ibi_df.drop([i for i in range(int(del_num['ibi']) + 1)], inplace=True)
-breathing_df.drop([i for i in range(int(del_num['breathing']))], inplace=True)
-ecg_df.drop([i for i in range(int(del_num['ecg']))], inplace=True)
+#breathing_df.drop([i for i in range(int(del_num['breathing']))], inplace=True)
+#ecg_df.drop([i for i in range(int(del_num['ecg']))], inplace=True)
 bvp_df.drop([i for i in range(int(del_num['bvp']))], inplace=True)
 
 # get the fastest end time in sec
-min_recorded_time_in_sec = min(len(hr_df)/hz['hr'], len(eda_df)/hz['eda'], len(ibi_df)/hz['ibi'], len(breathing_df)/hz['breathing'], len(ecg_df)/hz['ecg'], len(bvp_df)/hz['bvp'])
+min_recorded_time_in_sec = min(len(hr_df)/hz['hr'], len(eda_df)/hz['eda'], len(ibi_df)/hz['ibi'], len(bvp_df)/hz['bvp'])
 fastest_end_time = int(latest_time) + math.trunc(min_recorded_time_in_sec)
 
 
@@ -353,7 +389,8 @@ for i in range(-1, len(label)):
 	elif get_UTC_time(str(label['start'][max(i, 0)])) > get_UTC_time(str(label['end'][max(i, 0)])):
 		print("Your LABEL.csv row[", i, "] is invalid. (start time is later than end time)")
 		exit(0)
-	zephyr_time = int(zephyr_time)
+	#zephyr_time = int(zephyr_time)
+	zephyr_time = 0
 	# time interval before the first cell in LABEL.csv
 	if i == -1:
 		start_time = int(latest_time)
@@ -386,9 +423,9 @@ for i in range(-1, len(label)):
 
 data = {'labeling': labeling,
 		'hr_max': hr_max, 'hr_min': hr_min, 'hr_avg': hr_avg, 'hr_med': hr_med, 'hr_std': hr_std,
-		'eda_max': eda_max, 'eda_min': eda_min, 'eda_avg': eda_avg, 'eda_med': eda_med, 'eda_std': eda_std,
-		'breathing_max': breathing_max, 'breathing_min': breathing_min, 'breathing_avg': breathing_avg, 'breathing_med': breathing_med, 'breathing_std': breathing_std,
-		'ecg_max': ecg_max, 'ecg_min': ecg_min, 'ecg_avg': ecg_avg, 'ecg_med': ecg_med, 'ecg_std': ecg_std, 'hrv':hrv}
+		'eda_max': eda_max, 'eda_min': eda_min, 'eda_avg': eda_avg, 'eda_med': eda_med, 'eda_std': eda_std, 'sbp': sbp}
+		#'breathing_max': breathing_max, 'breathing_min': breathing_min, 'breathing_avg': breathing_avg, 'breathing_med': breathing_med, 'breathing_std': breathing_std,
+		#'ecg_max': ecg_max, 'ecg_min': ecg_min, 'ecg_avg': ecg_avg, 'ecg_med': ecg_med, 'ecg_std': ecg_std,
 
 all_dataFrame.append(pandas.DataFrame(data=data))
 
